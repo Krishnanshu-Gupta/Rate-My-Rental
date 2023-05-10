87017070dc2d5571bbd0c3e73a85fda689ee498a
hamzadeif <131315025+hdeif@users.noreply.github.com>
2023-05-10 12:19:28 -0700

    Set up MERN

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(
+  process.env.REACT_APP_MONGO_URI
+);
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
