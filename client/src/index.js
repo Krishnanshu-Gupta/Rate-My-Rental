7a931ec8826fec039d042c6d128f8a80dd0bec6c
Sidd <siddarthsv01@gmail.com>
2023-06-13 02:31:34 -0700

    Fixed the merge issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Reviews from "./Reviews";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Reviews />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
